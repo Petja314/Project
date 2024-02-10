14ea7ac1297de12e3733657dc3b4f15500c86fcd
dmitrijs <petja314@gmail.com>
2024-02-10 17:49:14 +0000

    Updates

--- src/react-app-env.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react-scripts" />
-interface ImportMeta {
-    readonly env: {
-        readonly REACT_APP_API_KEY: string;
-        readonly REACT_APP_API_KEY_NEWS : string
-        // Add other environment variables as needed
-    };
-}
